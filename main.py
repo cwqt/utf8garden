2311a3348257cfef1d2c6860b59e754645ed0fa5
Cass <m@cass.si>
2019-08-18 23:26:39 +0100

    use heroku scheduler instead of timer.sleep

--- main.py
+++ main.py
@@ -18,37 +18,36 @@ api = tweepy.API(auth)
 
 #max dimensions = 16x16 grid = 256 characters
 
-while True:
-	fmap = []
-	for y in range(0, 4):
-		fmap.append([])
-		for x in range(0, 4):
-			fmap[y].append(random.randint(0,8))
+fmap = []
+for y in range(0, 4):
+	fmap.append([])
+	for x in range(0, 4):
+		fmap[y].append(random.randint(0,8))
 
-	# horizontal flip
-	fmap2 = []
-	for y in fmap:
-		fmap2.append(y[::-1])
+# horizontal flip
+fmap2 = []
+for y in fmap:
+	fmap2.append(y[::-1])
 
-	# join
-	for y in range(len(fmap)):
-		fmap[y] = fmap[y] + fmap2[y]
+# join
+for y in range(len(fmap)):
+	fmap[y] = fmap[y] + fmap2[y]
 
-	#vertical flip
-	fmap2 = fmap.copy()
-	for y in reversed(fmap2):
-		fmap.append(y)
+#vertical flip
+fmap2 = fmap.copy()
+for y in reversed(fmap2):
+	fmap.append(y)
 
-	print("\n")
+print("\n")
 
-	plantmap = ['🌷','🌸','🌹','🌺','🌻','🌼','🌱','🌳','🌲']
-	string = ""
+plantmap = ['🌷','🌸','🌹','🌺','🌻','🌼','🌱','🌳','🌲']
+string = ""
 
-	for y in fmap:
-		for x in y:
-			string += str(plantmap[x])
-		string += "\n"
+for y in fmap:
+	for x in y:
+		string += str(plantmap[x])
+	string += "\n"
 
-	api.update_status(status=string)
-	print("Posted!")
-	time.sleep(3600)
+api.update_status(status=string)
+print("Posted!")
+print(string)
